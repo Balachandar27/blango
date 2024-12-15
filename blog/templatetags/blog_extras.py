3f7211043a554405a29e7f58c9140809790195b1
Balachandar Vinayagam <vin_balachandar@yahoo.com>
2024-12-15 03:30:23 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import format_html
+import logging
 
 register = template.Library()
+
+logger = logging.getLogger(__name__)
 
 user_model = get_user_model()
 
--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post
 from django.utils import timezone
 from blog.forms import CommentForm
+# from django.views.decorators.cache import cache_page
+# from django.views.decorators.vary import vary_on_cookie
+from django.http import HttpResponse
+import logging
+
 # Create your views here.
+# @cache_page(300)
+# @vary_on_cookie
 def index(request):
+    logger = logging.getLogger(__name__)
+    # return HttpResponse(str(request.user).encode("ascii"))
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
