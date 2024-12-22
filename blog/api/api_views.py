ddbf0d185b0e8e56a320905fcbe36286228f86fd
Balachandar Vinayagam <vin_balachandar@yahoo.com>
2024-12-22 22:05:17 +0000

    Finish filtering

--- blog/api/api_views.py
+++ blog/api/api_views.py
@@ -19,7 +19,10 @@ from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_headers, vary_on_cookie
 
 from rest_framework.exceptions import PermissionDenied
-
+from django.db.models import Q
+from django.utils import timezone
+from datetime import timedelta
+from django.http import Http404
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
@@ -59,8 +62,36 @@ class PostViewSet(viewsets.ModelViewSet):
             return PostSerializer
         return PostDetailSerializer
 
+    def get_queryset(self):
+        if self.request.user.is_anonymous:
+            # published only
+            queryset = self.queryset.filter(published_at__lte=timezone.now())
+
+        elif self.request.user.is_staff:
+            # allow all
+            queryset = self.queryset
+
+        else:
+        # filter for own or
+            queryset = self.queryset.filter(Q(published_at__lte=timezone.now()) | Q(author=self.request.user))
+        
+        time_period_name = self.kwargs.get("period_name")
+
+        if not time_period_name:
+            # no further filtering required
+            return queryset
+
+        if time_period_name == "new":
+            return queryset.filter(published_at__gte=timezone.now() - timedelta(hours=1))
+        elif time_period_name == "today":
+            return queryset.filter(published_at__date=timezone.now().date(),)
+        elif time_period_name == "week":
+            return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+        else:
+            raise Http404(f"Time period {time_period_name} is not valid, should be " f"'new', 'today' or 'week'")
+
     @method_decorator(cache_page(300))
-    @method_decorator(vary_on_headers("Authorization"))
+    @method_decorator(vary_on_headers("Authorization", "Cookie"))
     @method_decorator(vary_on_cookie)
     @action(methods=["get"], detail=False, name="Posts by the logged in user")
     def mine(self, request):
@@ -71,6 +102,7 @@ class PostViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
     
         @method_decorator(cache_page(120))
+        @method_decorator(vary_on_headers("Authorization", "Cookie"))
         def list(self, *args, **kwargs):
             return super(PostViewSet, self).list(*args, **kwargs)
 
